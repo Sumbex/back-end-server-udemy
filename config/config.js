c7ad7d14bfe5219e7310af79214d398f8cfd89b3
bvidalneofox <v_andres01@hotmail.com>
2020-06-09 16:23:58 -0400

    fin seccion 12

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'pedon-muma-exe';
+
+// Google
+module.exports.CLIENT_ID = '1077143188123-rbb16ef4u5m1djrbkauhnlak6ehuaobk.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -6,6 +6,113 @@ var SEED = require('../config/config').SEED;
 var app = express();
 var Usuario = require('../models/usuario');
 
+// Google
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+// Autenticacion de Google
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    // const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    // const domain = payload['hd'];
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+
+}
+verify().catch(console.error);
+
+app.post('/google', async (req, res) => {
+
+    var token = req.body.token;
+
+    var googleUser = await verify(token)
+        .catch(e => {
+            res.status(200).json({
+                ok: false,
+                mensaje: 'Token no valido'
+            });
+        });
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+
+        if (usuarioDB) {
+
+            if (!usuarioDB.google) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe de usar su autenticacino normal',
+                });
+            } else {
+
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); //4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    mensaje: 'Login correcto'
+                });
+
+            }
+
+        }else{
+
+            // Usuario no existe hay que crearlo
+            var usuario = new Usuario();
+
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+
+            usuario.save((err, usuario) => {
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); //4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuario,
+                    token: token,
+                    mensaje: 'Login correcto'
+                });
+
+            })
+
+        }
+
+    });
+
+    // res.status(200).json({
+    //     ok: true,
+    //     mensaje: 'Login correcto',
+    //     googleUser: googleUser
+    // });
+
+});
+
+// Autenticacion normal
 app.post('/', (req, res) => {
 
     var body = req.body;
@@ -26,7 +133,7 @@ app.post('/', (req, res) => {
             });
         }
 
-        if(!bcrypt.compareSync(body.password, usuarioDB.password)){
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
             return res.status(400).json({
                 ok: false,
                 mensaje: 'Contraseña incorrecta',
@@ -36,7 +143,7 @@ app.post('/', (req, res) => {
 
         // Crear un token
         usuarioDB.password = ':)';
-        var token = jwt.sign({usuario: usuarioDB}, SEED, {expiresIn: 14400}); //4 horas
+        var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); //4 horas
 
         res.status(200).json({
             ok: true,
